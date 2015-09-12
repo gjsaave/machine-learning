af4ec3517beb6fdd7ef336c5c82493874859ba23
gary <gary@192.168.10.101>
2015-09-12 15:28:35 -0600

    refactored.  added file writing

--- assignment1/src/assignment1/ChessSet.java
+++ assignment1/src/assignment1/ChessSet.java
@@ -3,6 +3,7 @@ package assignment1;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.PrintWriter;
 import java.net.URL;
 
 import weka.classifiers.Classifier;
@@ -37,116 +38,73 @@ public class ChessSet {
 		data.setClassIndex(data.numAttributes() - 1);
 		return data;
 	}
-
-	public void FPRDecisionTreeTraining(Instances newData) throws Exception {
-		Instances train = newData;
-		//Instances test = returnTestSet();
-		// train classifier
-		Classifier cls = new J48();
-		cls.buildClassifier(train);
-		// evaluate classifier and print some statistics
-		Evaluation eval = new Evaluation(train);
-		//Evaluation eval_train = new Evaluation(train);
-		eval.evaluateModel(cls, train);
-		//eval_train.evaluateModel(cls, train);
-		//System.out.println("Training FPR");
-		//System.out.println(eval_train.falsePositiveRate(0));
-		//System.out.println("Test FPR");
-		System.out.println(eval.falsePositiveRate(0));
-
-	}
 	
-	public void FPRDecisionTreeTest(Instances newData) throws Exception {
-		Instances train = newData;
-		Instances test = returnTestSet();
-		// train classifier
-		Classifier cls = new J48();
-		cls.buildClassifier(train);
-		// evaluate classifier and print some statistics
-		Evaluation eval = new Evaluation(train);
-		Evaluation eval_train = new Evaluation(train);
-		eval.evaluateModel(cls, test);
-		eval_train.evaluateModel(cls, train);
-		//System.out.println("Training FPR");
-		//System.out.println(eval_train.falsePositiveRate(0));
-		//System.out.println("Test FPR");
-		System.out.println(eval.falsePositiveRate(0));
-
-	}
-	
-	public void FNRDecisionTreeTraining(Instances newData) throws Exception {
-		Instances train = newData;
-		//Instances test = returnTestSet();
-		// train classifier
-		Classifier cls = new J48();
-		cls.buildClassifier(train);
-		// evaluate classifier and print some statistics
-		Evaluation eval = new Evaluation(train);
-		//Evaluation eval_train = new Evaluation(train);
-		eval.evaluateModel(cls, train);
-		//eval_train.evaluateModel(cls, train);
-		//System.out.println("Training FPR");
-		//System.out.println(eval_train.falsePositiveRate(0));
-		//System.out.println("Test FPR");
-		System.out.println(eval.falsePositiveRate(0));
-
-	}
-	
-	public void FNRDecisionTreeTest(Instances newData) throws Exception {
-		Instances train = newData;
-		Instances test = returnTestSet();
-		// train classifier
-		Classifier cls = new J48();
-		cls.buildClassifier(train);
-		// evaluate classifier and print some statistics
-		Evaluation eval = new Evaluation(train);
-		eval.evaluateModel(cls, test);
-		//System.out.println("Test FNR");
-		System.out.println(eval.falseNegativeRate(0));
-
-	}
-	
-	public void setPercentDecisionTree() throws Exception{
-		//Evaluation eval = decisionTree();
+	public void FPRDecisionTreeTraining() throws Exception {
 		Instances train = returnTrainingSet();
+		Instances test = returnTestSet();
+		// train classifier
+		Classifier cls = new J48();
+		PrintWriter out = new PrintWriter("TrainingFPR.dat");
+		for (int i=10; i>-1; i--){
+			int index = 10-i;
+			Instances newData = setPercentDecisionTree(train, i);
+			cls.buildClassifier(newData);
+			Evaluation eval = new Evaluation(newData);
+			eval.evaluateModel(cls, newData);
+			out.println(index + "\t" + eval.falsePositiveRate(0));
+		}
+		out.close();
+		
+		out = new PrintWriter("TestFPR.dat");
+		for (int i=10; i>-1; i--){
+			int index = 10-i;
+			Instances newData = setPercentDecisionTree(train, i);
+			cls.buildClassifier(newData);
+			Evaluation eval = new Evaluation(newData);
+			eval.evaluateModel(cls, test);
+			out.println(index + "\t" + eval.falsePositiveRate(0));
+		}
+		out.close();
+
+		out = new PrintWriter("TrainingFNR.dat");
+		for (int i=10; i>-1; i--){
+			int index = 10-i;
+			Instances newData = setPercentDecisionTree(train, i);
+			cls.buildClassifier(newData);
+			Evaluation eval = new Evaluation(newData);
+			eval.evaluateModel(cls, newData);
+			out.println(index + "\t" + eval.falseNegativeRate(0));
+		}
+		out.close();
+
+		out = new PrintWriter("TestFNR.dat");
+		for (int i=10; i>-1; i--){
+			int index = 10-i;
+			Instances newData = setPercentDecisionTree(train, i);
+			cls.buildClassifier(newData);
+			Evaluation eval = new Evaluation(newData);
+			eval.evaluateModel(cls, test);
+			out.println(index + "\t" + eval.falseNegativeRate(0));
+		}
+		out.close();	
+	}
+	
+	
+	public Instances setPercentDecisionTree(Instances train, int removeAmount) throws Exception {
+		// Evaluation eval = decisionTree();
 		RemovePercentage remove = new RemovePercentage();
-		System.out.println("Training FPR");
-		for (int i=10; i>-1; i--){
-			remove.setInputFormat(train);
-			remove.setPercentage(i*10);
-			Instances newData = Filter.useFilter(train, remove);
-			FPRDecisionTreeTraining(newData);	
-		}
-		
-		System.out.println("Test FPR");
-		for (int i=10; i>-1; i--){
-			remove.setInputFormat(train);
-			remove.setPercentage(i*10);
-			Instances newData = Filter.useFilter(train, remove);
-			FPRDecisionTreeTest(newData);	
-		}
-		
-		System.out.println("Training FNR");
-		for (int i=10; i>-1; i--){
-			remove.setInputFormat(train);
-			remove.setPercentage(i*10);
-			Instances newData = Filter.useFilter(train, remove);
-			FNRDecisionTreeTraining(newData);	
-		}
-		
-		System.out.println("Test FNR");
-		for (int i=10; i>-1; i--){
-			remove.setInputFormat(train);
-			remove.setPercentage(i*10);
-			Instances newData = Filter.useFilter(train, remove);
-			FNRDecisionTreeTest(newData);	
-		}
+		// System.out.println("Training FPR");
+		remove.setInputFormat(train);
+		remove.setPercentage(removeAmount * 10);
+		Instances newData = Filter.useFilter(train, remove);
+		return newData;
+
 	}
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		ChessSet cs = new ChessSet();
-		cs.setPercentDecisionTree();
+		cs.FPRDecisionTreeTraining();
 
 	}
 
